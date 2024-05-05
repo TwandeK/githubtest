76dc4e1d4b994bab685b5e9543d1e6cb329ad6a4
Soullessgame <twan.dekorte@gmail.com>
2024-05-05 13:58:17 +0200

    add calculation

---
+++ main.py
@@ -0,0 +1,19 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def plus(num1, num2):
+    total = num1 + num2
+
+    return total
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(plus(1,2))
+
